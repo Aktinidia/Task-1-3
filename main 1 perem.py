3d40febf1538a1214ce4de30c3d878c88caa8e60
Aktinidia <nata_kul@mail.ru>
2024-08-12 21:13:48 +0300

    Add files via upload

---
+++ main 1 perem.py
@@ -0,0 +1,5 @@
+amount_of_tasks = 12
+amount_of_hours = 1.5
+name_of_the_course = 'Python'
+one_task_time = amount_of_hours / amount_of_tasks
+print('Курс: ', name_of_the_course, ' Всего задач: ', amount_of_tasks, ' Затрачено часов: ', amount_of_hours, ' Среднее время выполнения: ',  one_task_time, 'часов')
